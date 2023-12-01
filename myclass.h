18beb6d3e978d61e9a5adedd10c3aa31d602b8d6
Joe Walton <152475586+earths-vase@users.noreply.github.com>
2023-12-01 20:48:20 +0600

    Create myclass.h

---
+++ myclass.h
@@ -0,0 +1,11 @@
+// myclass.h
+
+#ifndef MYCLASS_H  // Include guard to prevent multiple inclusion
+#define MYCLASS_H
+
+class MyClass {
+public:
+    void printMessage();
+};
+
+#endif // MYCLASS_H
